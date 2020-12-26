901214e755e8ea4bf4eb85088f0c529a8fe72737
joagxf <46498114+joagxf@users.noreply.github.com>
2020-12-26 16:33:30 -0500

    Add files via upload
    Impresion de un cuadrado en Python

---
+++ Cuadrado.py
@@ -0,0 +1,8 @@
+#CUADRADO
+
+print("Programa que te imprime 1 cuadrado\n","============================")
+
+print("+" + "*" * 10 + "+")
+print(("*" + " " *  8 + "  *\n") * 5, end ="")
+print("*" + " " *  8 + "  *") 
+print("+" + "*" * 10 + "+")
