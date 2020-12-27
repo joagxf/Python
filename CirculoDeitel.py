648211b4d6f9909ad1e2475a818457eb2bebf109
joagxf <46498114+joagxf@users.noreply.github.com>
2020-12-26 23:17:14 -0500

    Add files via upload

---
+++ CirculoDeitel.py
@@ -0,0 +1,8 @@
+#Circulo tomado dellibro de deitel and deitel
+
+print(  "   ***    ")
+print(" *" + " " * 5 + "*")
+print(("*" +  " " * 7 + "*\n") * 5, end = " ")
+print("*" + " " * 5 + "*")
+print(  "   ***    ")
+
---
+++ RectaguloDeitel.py
@@ -0,0 +1,6 @@
+#Rectangulo libro Deitel
+print("*" * 10 )
+print(("*" + " " * 8 + "*\n") * 7, end="")
+print("*" * 10 )
+
+
